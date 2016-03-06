fb88932a3cea46b380d7c562558e950bb0aee039
Rémy de Sérésin <latop2604@hotmail.com>
2016-03-06 21:20:07 +0100

    Bump version

--- UTorrent.Api.Data/Properties/AssemblyInfo.cs
+++ UTorrent.Api.Data/Properties/AssemblyInfo.cs
@@ -27,5 +27,5 @@ using System.Resources;
 // Vous pouvez spécifier toutes les valeurs ou utiliser par défaut les numéros de build et de version 
 // en utilisant '*', comme indiqué ci-dessous :
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.3")]
-[assembly: AssemblyFileVersion("1.0.0.3")]
+[assembly: AssemblyVersion("1.0.0.4")]
+[assembly: AssemblyFileVersion("1.0.0.4")]
